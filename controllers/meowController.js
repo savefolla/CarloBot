1a4c5b5a00522bc8ba7aa7570980e9b7ba5dce6c
savefolla <savefolla@gmail.com>
2017-07-15 17:30:37 +0200

    aggiutni comandi

---
+++ controllers/meowController.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Telegram = require('telegram-node-bot');
+
+class MeowController extends Telegram.TelegramBaseController {
+	handle($) {
+		var hours = new Date($.message.date*1000).getHours();
+		if(hours > 19 || hours < 7){
+			$.sendMessage('è ubriaca la Alessia ora');
+		}else{
+			$.sendMessage('Aspetto: 7.5\nCarattere: 6.9\nCapacità: 7');
+		}
+	}
+}
+
+module.exports = MeowController;
--- index.js
+++ index.js
@@ -12,7 +12,15 @@ const OtherwiseController = require('./controllers/otherwiseController');
 
 //per comandi validi
 const RandomController = require('./controllers/randomController');
+const MeowController = require('./controllers/meowController');
+const CustomFilterCommand = require('./node_modules/telegram-node-bot/lib/routing/commands/CustomFilterCommand');
 
 // routing
-tg.router.when(new Telegram.TextCommand('/random'), new RandomController())
+tg.router
+	.when(new Telegram.TextCommand('/random'), new RandomController())
+	.when(new CustomFilterCommand($ => {
+		return $.message.text.includes('@Meow958')
+	}), new MeowController())
+	.when(new Telegram.TextCommand('/Alessia'), new MeowController())
 	.otherwise(new OtherwiseController());
+
