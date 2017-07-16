76d9ca715a7dd2435bbea37ebe27d35d5d8c8e8e
savefolla <savefolla@gmail.com>
2017-07-16 09:07:26 +0200

    inzio db

--- controllers/randomController.js
+++ controllers/randomController.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const Telegram = require('telegram-node-bot');
+const db = require('../db/db');
 
 class RandomController extends Telegram.TelegramBaseController {
 	handle($) {
-		$.sendMessage('Messaggio casuale'); // riscrivere in modo che mandi un messaggio casuale
+		var n = Math.floor(Math.random()*db.length);
+		$.sendPhoto(Telegram.InputFile.byFilePath(db[n].link));
+		// $.sendMessage('Messaggio casuale'); // riscrivere in modo che mandi un messaggio casuale
 	}
 }
 
---
+++ db/db.js
@@ -0,0 +1,14 @@
+
+var db = [
+	{ 'type': 'photo',
+		'link': 'db/assets/223031827_702914.jpg'
+	},
+	{ 'type': 'photo',
+		'link': 'db/assets/226606080_110384.jpg'
+	},
+	{ 'type': 'photo',
+		'link': 'db/assets/230130922_93088.jpg'
+	}
+];
+
+module.exports = db;
