de088e5944f8747866ddd1e0712ea9a43d5dd517
savefolla <savefolla@gmail.com>
2017-07-19 15:15:00 +0200

    ripulito codice

--- index.js
+++ index.js
@@ -55,116 +55,68 @@ tg.router
 	.when(new Telegram.TextCommand('/random'), new RandomController())
 	.when(new Telegram.TextCommand('/donate'), new DonateController())
 	.when(new CustomFilterCommand($ => {
-		if($.message.text != null && tg.sveglio == true) {
-			return $.message.text.includes('@Meow958');
-		} else {
-			return false;
-		};
+		return $.message.text != null && 
+		tg.sveglio == true &&
+		$.message.text.includes('@Meow958');
 	}), new MeowController())
 	.when(new CustomFilterCommand($ => {
-		if($.message.text != null && tg.sveglio == true) {
-			return $.message.text.includes('scelta') || $.message.text.includes('Scelta') ||
-						$.message.text.includes('scelta') || $.message.text.includes('Scelta') ||
-						$.message.text.includes('scelto') || $.message.text.includes('Scelto') ||
-						$.message.text.includes('Ho') || $.message.text.includes('ho') ||
-						$.message.text.includes('abbiamo') || $.message.text.includes('Abbiamo') ||
-						$.message.text.includes('scelgo') || $.message.text.includes('Scelgo') ||
-						$.message.text.includes('sceglierò') || $.message.text.includes('Sceglierò') ||
-						$.message.text.includes('scegliere') || $.message.text.includes('Scegliere')
-		} else {
-			return false;
-		}
+		return $.message.text != null && tg.sveglio == true &&
+		($.message.text.includes('scelta') || $.message.text.includes('Scelta') ||
+		$.message.text.includes('scelta') || $.message.text.includes('Scelta') ||
+		$.message.text.includes('scelto') || $.message.text.includes('Scelto') ||
+		$.message.text.includes('Ho') || $.message.text.includes('ho') ||
+		$.message.text.includes('abbiamo') || $.message.text.includes('Abbiamo') ||
+		$.message.text.includes('scelgo') || $.message.text.includes('Scelgo') ||
+		$.message.text.includes('sceglierò') || $.message.text.includes('Sceglierò') ||
+		$.message.text.includes('scegliere') || $.message.text.includes('Scegliere'))
 	}, 'sceltatua'), new GambeController())
 	.when(new CustomFilterCommand($ => {
-		if($.message.text != null && tg.sveglio == true) {
-			return $.message.text == 'ti' || $.message.text == 'Ti'
-		} else {
-			return false;
-		}
+		return $.message.text != null && tg.sveglio == true && 
+		($.message.text == 'ti' || $.message.text == 'Ti'|| $.message.text == 'TI')
 	}, 'spezzo'), new GambeController())
 	.when(new CustomFilterCommand($ => {
-		if($.message.text != null && tg.sveglio == true) {
-			return $.message.text == 'ti spezzo' || $.message.text == 'Ti spezzo'
-		} else {
-			return false;
-		}
+		return $.message.text != null && tg.sveglio == true &&
+		($.message.text == 'ti spezzo' || $.message.text == 'Ti spezzo')
 	}, 'le'), new GambeController())
 	.when(new CustomFilterCommand($ => {
-		if($.message.text != null && tg.sveglio == true) {
-			return $.message.text == 'ti spezzo le' || $.message.text == 'Ti spezzo le'
-		} else {
-			return false;
-		}
+		return $.message.text != null && tg.sveglio == true &&
+		($.message.text == 'ti spezzo le' || $.message.text == 'Ti spezzo le')
 	}, 'gambe'), new GambeController())
 	.when(new CustomFilterCommand($ => {
-		if($.message.text != null && tg.sveglio == true) {
-			return $.message.text == 'ti spezzo le gambe' || $.message.text == 'Ti spezzo le gambe'
-		} else {
-			return false;
-		}
+		return $.message.text != null && tg.sveglio == true &&
+		($.message.text == 'ti spezzo le gambe' || $.message.text == 'Ti spezzo le gambe')
 	}, 'sceltatua'), new GambeController())
 	.when(new CustomFilterCommand($ => {
-		if($.message.text != null && tg.sveglio == true) {
-			return $.message.text.includes(' Carlo ') || $.message.text.includes(' carlo ') || $.message.text.includes('@carlocingolato') || $.message.text.includes('@uomocingolato');
-		} else {
-			return false;
-		};
+		return $.message.text != null && tg.sveglio == true &&
+		($.message.text.includes(' Carlo ') || $.message.text.includes(' carlo ') || $.message.text.includes('@carlocingolato') || $.message.text.includes('@uomocingolato'))
 	}), new CarloController())
 	.when(new CustomFilterCommand($ => {
-		if($.message.text != null && tg.sveglio == true) {
-			return $.message.text.includes('swe');
-		} else {
-			return false;
-		};
+		return $.message.text != null && tg.sveglio == true &&
+		$.message.text.includes('swe')
 	}), new SweController())
 	.when(new CustomFilterCommand($ => {
-		if($.message.text != null && tg.sveglio == true) {
-			return $.message.text.includes('p2');
-		} else {
-			return false;
-		};
+		return $.message.text != null && tg.sveglio == true &&
+		$.message.text.includes('p2')
 	}), new P2Controller())
 	.when(new CustomFilterCommand($ => {
-		if($.message.text != null && tg.sveglio == true) {
-			return $.message.text.includes('p3');
-		} else {
-			return false;
-		};
+		return $.message.text != null && tg.sveglio == true &&
+		$.message.text.includes('p3')
 	}), new P3Controller())
 	.when(new CustomFilterCommand($ => {
-		if($.message.text != null && tg.sveglio == true) {
-			return $.message.text.includes('😂') || $.message.text.includes('haha') || $.message.text.includes('lol') || $.message.text.includes('lel') ;
-		} else {
-			return false;
-		};
+		return $.message.text != null && tg.sveglio == true &&
+		($.message.text.includes('😂') || $.message.text.includes('haha') || $.message.text.includes('lol') || $.message.text.includes('lel'))
 	}), new RidiController())
 	.when(new CustomFilterCommand($ => {
-		if($.message.text && tg.sveglio == true) {
-			return true;
-		}else{
-			return false;
-		};
+		return $.message.text && tg.sveglio == true
 	}), new SituationController())
 	.when(new CustomFilterCommand($ => {
-		if($.message.from.id == '17694064' && tg.sveglio == true) {
-			return true;
-		}else{
-			return false;
-		};
+		return $.message.from.id == '17694064' && tg.sveglio == true
 	}), new FromCarloController())
 	.when(new CustomFilterCommand($ => {
-		if($.message.text != null && $.message.text.includes('ok' && tg.sveglio == true)) {
-			return true;
-		}else{
-			return false;
-		};
+		return $.message.text != null && $.message.text.includes('ok' && tg.sveglio == true)
 	}, 'sceltatua'), new GambeController())
 	.when(new CustomFilterCommand($ => {
-		if($.message.text != null && $.message.text.includes('altrimenti') && tg.sveglio == true) {
-			return true;
-		}else{
-			return false;
-		};
+		return $.message.text != null && $.message.text.includes('altrimenti') && tg.sveglio == true
 	}), new AltrimentiController())
 	.when(new CustomFilterCommand($ => {
 		return $.message.chat.username == 'savefolla' || $.message.chat.username == 'Sparkolo' || $.message.chat.username == 'Meow958' || $.message.chat.username == 'ohalbero';
