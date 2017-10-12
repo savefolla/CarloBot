015faadc0d6d7de4e248267b8c7b30d383365aee
Saverio Follador <saverio.follador@moku.io>
2017-10-12 09:36:34 +0200

    token

--- token.js
+++ token.js
@@ -1,5 +1,5 @@
 
-//module.exports = '414303977:AAGiHNSESqXFsU2GkjchuzUoYMmlgQNofOA';
+module.exports = '414303977:AAGiHNSESqXFsU2GkjchuzUoYMmlgQNofOA';
 
 
-module.exports = '420986361:AAFZUcnF9wf-pbizvYQYkuCNuv75RAaYago';
+//module.exports = '420986361:AAFZUcnF9wf-pbizvYQYkuCNuv75RAaYago';
