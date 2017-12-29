b865cfb27c30247220880655f2e02145089319ea
Saverio Follador <saverio.follador@moku.io>
2017-12-29 16:10:48 +0100

    update

--- controllers/getMsg.js
+++ controllers/getMsg.js
@@ -1,11 +1,29 @@
 'use strict';
 
 const Telegram = require('telegram-node-bot');
+const messages = require('../db/messages.json');
 
+const milliseconds = 1000*60*60*24;
 
 class GetMsg extends Telegram.TelegramBaseController {
 	handle($) {
-		console.log($);
+		//console.log($);
+		this.clean($);
+	}
+	clean($) {
+		for (let i = 0; i < messages.length; ++i) {
+			if (new Date().getTime() > messages[i].date + milliseconds) {
+                this.deleteMessage($, messages[i]);
+                messages.splice(i, 1);
+            }
+		}
+	}
+	deleteMessage($, message) {
+        // $.api.call as a workaround for the absence of deleteMessage method
+        $.api.call('deleteMessage', Object.assign({
+            chat_id: message.chat_id,
+            message_id: message.message_id
+        }));
 	}
 }
 
--- controllers/sendMessage.js
+++ controllers/sendMessage.js
@@ -1,62 +1,87 @@
 
-const milliseconds = 1000*60*60*24;
+const messages = require('../db/messages.json');
+const fs = require('fs');
 
 exports.sendPhoto = ($, message) => {
     $.sendPhoto(message).then((res) => {
-        // $.api.call as a workaround for the absence of deleteMessage method
-        setTimeout(() => $.api.call('deleteMessage', Object.assign({
+        messages.push({
             chat_id: $.message.chat.id,
-            message_id: res.messageId
-        })), milliseconds);
+            message_id: res.messageId,
+            date: new Date().getTime()
+        });
+        fs.writeFile('db/messages.json', JSON.stringify(messages), function(err) {
+            if(err) console.log(err);
+            else console.log('messages db updated')
+        });
     });
 }
 
 exports.sendVoice = ($, message) => {
     $.sendVoice(message).then((res) => {
-        // $.api.call as a workaround for the absence of deleteMessage method
-        setTimeout(() => $.api.call('deleteMessage', Object.assign({
+        messages.push({
             chat_id: $.message.chat.id,
-            message_id: res.messageId
-        })), milliseconds);
+            message_id: res.messageId,
+            date: new Date().getTime()
+        });
+        fs.writeFile('db/messages.json', JSON.stringify(messages), function(err) {
+            if(err) console.log(err);
+            else console.log('messages db updated')
+        });
     });
 }
 
 exports.sendAudio = ($, message) => {
     $.sendAudio(message).then((res) => {
-        // $.api.call as a workaround for the absence of deleteMessage method
-        setTimeout(() => $.api.call('deleteMessage', Object.assign({
+        messages.push({
             chat_id: $.message.chat.id,
-            message_id: res.messageId
-        })), milliseconds);
+            message_id: res.messageId,
+            date: new Date().getTime()
+        });
+        fs.writeFile('db/messages.json', JSON.stringify(messages), function(err) {
+            if(err) console.log(err);
+            else console.log('messages db updated')
+        });
     });
 }
 
 exports.sendVideo = ($, message) => {
     $.sendVideo(message).then((res) => {
-        // $.api.call as a workaround for the absence of deleteMessage method
-        setTimeout(() => $.api.call('deleteMessage', Object.assign({
+        messages.push({
             chat_id: $.message.chat.id,
-            message_id: res.messageId
-        })), milliseconds);
+            message_id: res.messageId,
+            date: new Date().getTime()
+        });
+        fs.writeFile('db/messages.json', JSON.stringify(messages), function(err) {
+            if(err) console.log(err);
+            else console.log('messages db updated')
+        });
     });
 }
 
 exports.sendMessage = ($, message) => {
     $.sendMessage(message).then((res) => {
-        // $.api.call as a workaround for the absence of deleteMessage method
-        setTimeout(() => $.api.call('deleteMessage', Object.assign({
+        messages.push({
             chat_id: $.message.chat.id,
-            message_id: res.messageId
-        })), milliseconds);
+            message_id: res.messageId,
+            date: new Date().getTime()
+        });
+        fs.writeFile('db/messages.json', JSON.stringify(messages), function(err) {
+            if(err) console.log(err);
+            else console.log('messages db updated')
+        });
     });
 }
 
 exports.sendSticker = ($, message) => {
     $.sendSticker(message).then((res) => {
-        // $.api.call as a workaround for the absence of deleteMessage method
-        setTimeout(() => $.api.call('deleteMessage', Object.assign({
+        messages.push({
             chat_id: $.message.chat.id,
-            message_id: res.messageId
-        })), milliseconds);
+            message_id: res.messageId,
+            date: new Date().getTime()
+        });
+        fs.writeFile('db/messages.json', JSON.stringify(messages), function(err) {
+            if(err) console.log(err)
+            else console.log('messages db updated')
+        });
     });
 }
